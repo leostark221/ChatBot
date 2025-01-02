451df398ae1ff37a8967899d1a1ea2b453d9223c
leostark221 <ashair70@gmail.com>
2025-01-02 15:17:43 +0500

    made ui changes

--- app.py
+++ app.py
@@ -1,72 +1,72 @@
-# from flask import Flask, render_template, jsonify, request
-# from src.helper import download_hugging_face_embeddings
-# from langchain_pinecone import PineconeVectorStore
-# from langchain_openai import OpenAI
-# from langchain.chains import create_retrieval_chain
-# from langchain.chains.combine_documents import create_stuff_documents_chain
-# from langchain_core.prompts import ChatPromptTemplate
-# from dotenv import load_dotenv
-# from src.prompt import *
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
 # import os
 
-# app = Flask(__name__)
+# # Directly assign the API keys here
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-# load_dotenv()
-
-# PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-# OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-
-# # import os
-
-# # # Directly assign the API keys here
-# os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-# os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-
-# embeddings = download_hugging_face_embeddings()
+embeddings = download_hugging_face_embeddings()
 
 
-# index_name = "mentalhealth-bot"
+index_name = "mentalhealth-bot"
 
-# # Embed each chunk and upsert the embeddings into your Pinecone index.
-# docsearch = PineconeVectorStore.from_existing_index(
-#     index_name=index_name,
-#     embedding=embeddings
-# )
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
 
-# retriever = docsearch.as_retriever(
-#     search_type="similarity", search_kwargs={"k": 3})
-
-
-# llm = OpenAI(temperature=0.4, max_tokens=500)
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", system_prompt),
-#         ("human", "{input}"),
-#     ]
-# )
-
-# question_answer_chain = create_stuff_documents_chain(llm, prompt)
-# rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+retriever = docsearch.as_retriever(
+    search_type="similarity", search_kwargs={"k": 3})
 
 
-# @app.route("/")
-# def index():
-#     return render_template('chat.html')
+llm = OpenAI(temperature=0.4, max_tokens=500)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
 
-# @app.route("/get", methods=["GET", "POST"])
-# def chat():
-#     msg = request.form["msg"]
-#     input = msg
-#     print(input)
-#     response = rag_chain.invoke({"input": msg})
-#     print("Response : ", response["answer"])
-#     return str(response["answer"])
+@app.route("/")
+def index():
+    return render_template('chat.html')
 
 
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 8080))  # Default to 8080
-#     app.run(host="0.0.0.0", port=port)
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080
+    app.run(host="0.0.0.0", port=port)
 
 
 # from flask import Flask, render_template
