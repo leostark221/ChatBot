56353c97d2ec8a5fc347e844d7baa090d7dbf38f
leostark221 <ashair70@gmail.com>
2025-01-02 09:23:16 +0500

    changed port

--- app.py
+++ app.py
@@ -65,8 +65,8 @@ def chat():
 
 
 if __name__ == '__main__':
-    # Remove any hardcoded port, as it's unnecessary for Render
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080
+    app.run(host="0.0.0.0", port=port)
 
 
 # from flask import Flask, render_template
